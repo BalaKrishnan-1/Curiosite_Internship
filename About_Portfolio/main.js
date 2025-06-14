423f87f542e6ea839f96a7de477a8f51614b3f6c
Balakrishnan-1 <bk4922813@gmail.com>
2025-06-14 12:37:04 +0530

    Feat: Updated html and Css , Integrated Js

---
+++ About_Portfolio/main.js
@@ -0,0 +1,6 @@
+const toggler= document.getElementById('toggler')
+const unorderedlist =document.getElementById('unorder')
+
+    toggler.addEventListener('click',()=>{
+        unorderedlist.classList.toggle('active')
+    })
